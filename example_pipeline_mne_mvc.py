83807294f078096a5b9765e095df04213b89cb90
Thomas Samuel Binns <t.s.binns@outlook.com>
2022-10-13 17:32:00 +0200

    bug fix

--- example_pipeline_mne_mvc.py
+++ example_pipeline_mne_mvc.py
@@ -11,7 +11,7 @@ from mne_connectivity import (
 data = read_epochs("Data/epochs-epo.fif")
 
 ## Load analysis settings
-with open("Settings/pipeline_settings_mne.json", encoding="utf-8") as settings_file:
+with open("Settings/pipeline_settings_mne_mvc.json", encoding="utf-8") as settings_file:
     settings = json.load(settings_file)
 
 ## Compute connectivity
